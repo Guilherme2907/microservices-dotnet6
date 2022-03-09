97aac15cb5c7e7f83788893968901d5f4ee9f113
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2022-03-08 23:02:12 -0300

    Finishing module 7 - Configuring the database with EF Core and creating the first entity

---
+++ S07_Microservices.NET6/GeekShopping/GeekShopping.ProductAPI/Model/Context/SqlServerContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class SqlServerContext : DbContext
+    {
+        public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options)
+        {
+        }
+
+        protected SqlServerContext() { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
