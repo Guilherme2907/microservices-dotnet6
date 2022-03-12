23d0e6771a499e6fc8472ef24f120becd0001c38
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2022-03-12 16:11:03 -0300

    Finishing module 9 - Defining the layout and consuming the products microservice

---
+++ S09_Microservices.NET6/GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindBydAll()
+        {
+            var products = await _productRepository.FindAll();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindBydId(long id)
+        {
+            var product = await _productRepository.FindById(id);
+
+            if (product == null) return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create([FromBody] ProductVO productVO)
+        {
+            if(productVO == null) return NotFound();
+
+            var product = await _productRepository.Create(productVO);
+
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update([FromBody] ProductVO productVO)
+        {
+            if (productVO == null) return NotFound();
+
+            var product = await _productRepository.Update(productVO);
+
+            return Ok(product);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var status = await _productRepository.Delete(id);
+
+            if(!status) return BadRequest();
+
+            return Ok(status);
+        }
+    }
+}
