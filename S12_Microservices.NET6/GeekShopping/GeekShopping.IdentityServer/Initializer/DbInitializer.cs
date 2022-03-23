a4887f9b87dca7484494e77fe4508d2c6d4a7b5f
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2022-03-22 22:13:06 -0300

    Adding product listing to home

---
+++ S12_Microservices.NET6/GeekShopping/GeekShopping.IdentityServer/Initializer/DbInitializer.cs
@@ -0,0 +1,74 @@
+﻿using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace GeekShopping.IdentityServer.Initializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly SqlServerContext _context;
+        private readonly UserManager<ApplicationUser> _user;
+        private readonly RoleManager<IdentityRole> _role;
+
+        public DbInitializer(SqlServerContext context, UserManager<ApplicationUser> user, RoleManager<IdentityRole> role)
+        {
+            _context = context;
+            _user = user;
+            _role = role;
+        }
+
+        public async Task Initialize()
+        {
+            if (await _role.FindByNameAsync(IdentityConfiguration.ADMIN) != null) return;
+
+            await _role.CreateAsync(new IdentityRole(IdentityConfiguration.ADMIN));
+            await _role.CreateAsync(new IdentityRole(IdentityConfiguration.CLIENT));
+
+
+            ApplicationUser admin = new ApplicationUser()
+            {
+                UserName = "guilherme-admin",
+                Email = "guilherme-admin@geekshopping.com",
+                EmailConfirmed = true,
+                PhoneNumber = "+55 (99) 99999-9999",
+                FirstName = "Guilherme",
+                LastName = "Admin"
+            };
+
+            await _user.CreateAsync(admin, "Guiadm123@");
+            await _user.AddToRoleAsync(admin, IdentityConfiguration.ADMIN);
+
+            ApplicationUser client = new ApplicationUser()
+            {
+                UserName = "guilherme-client",
+                Email = "guilherme-client@geekshopping.com",
+                EmailConfirmed = true,
+                PhoneNumber = "+55 (99) 99999-8888",
+                FirstName = "Guilherme",
+                LastName = "Client"
+            };
+
+            await _user.CreateAsync(client, "Guiclient123@");
+            await _user.AddToRoleAsync(client, IdentityConfiguration.CLIENT);
+
+            await _user.AddClaimsAsync(admin, new Claim[]
+            {
+                new Claim(JwtClaimTypes.Name, $"{admin.FirstName} {admin.LastName}"),
+                new Claim(JwtClaimTypes.GivenName, admin.FirstName),
+                new Claim(JwtClaimTypes.FamilyName, admin.LastName),
+                new Claim(JwtClaimTypes.Role, IdentityConfiguration.ADMIN)
+            });
+
+            await _user.AddClaimsAsync(client, new Claim[]
+            {
+                new Claim(JwtClaimTypes.Name, $"{client.FirstName} {client.LastName}"),
+                new Claim(JwtClaimTypes.GivenName, client.FirstName),
+                new Claim(JwtClaimTypes.FamilyName, client.LastName),
+                new Claim(JwtClaimTypes.Role, IdentityConfiguration.CLIENT)
+            });
+        }
+    }
+}
---
+++ S12_Microservices.NET6/GeekShopping/GeekShopping.Web/Services/ProductService.cs
@@ -0,0 +1,62 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using GeekShopping.Web.Utils;
+using System.Net.Http.Headers;
+
+namespace GeekShopping.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+        public const string BASE_PATH = "api/v1/product";
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<IEnumerable<ProductModel>> FindAllProducts(string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await _httpClient.GetAsync(BASE_PATH);
+
+            return await response.ReadContentAs<List<ProductModel>>();
+        }
+        public async Task<ProductModel> FindProductById(long id, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await _httpClient.GetAsync($"{BASE_PATH}/{id}");
+
+            return await response.ReadContentAs<ProductModel>();
+        }
+
+        public async Task<ProductModel> CreateProduct(ProductModel product, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await _httpClient.PostAsJson(BASE_PATH, product);
+
+            return await response.ReadContentAs<ProductModel>();
+        }
+
+        public async Task<ProductModel> UpdateProduct(ProductModel product, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await _httpClient.PutAsJson(BASE_PATH, product);
+
+            return await response.ReadContentAs<ProductModel>();
+        }
+
+        public async Task<bool> DeleteProductById(long id, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await _httpClient.DeleteAsync($"{BASE_PATH}/{id}");
+
+            return await response.ReadContentAs<bool>();
+        }
+    }
+}
