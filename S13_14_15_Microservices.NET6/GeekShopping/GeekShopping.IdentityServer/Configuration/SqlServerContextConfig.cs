32b6578f8dbbde12cfc5974227474d64ef937cc8
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2022-03-31 21:27:11 -0300

    Finishing modules 13,14 and 15 - Implementing shopping cart microservice,Consuming shopping cart microservice and implementing shopping cart screen

---
+++ S13_14_15_Microservices.NET6/GeekShopping/GeekShopping.IdentityServer/Configuration/SqlServerContextConfig.cs
@@ -0,0 +1,17 @@
+﻿using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.IdentityServer.Configuration
+{
+    public static class SqlServerContextConfig
+    {
+        public static IServiceCollection ConfigureSqlServerContext(this IServiceCollection services, 
+                                                                        IConfiguration configuration)
+        {
+            services.AddDbContext<SqlServerContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
